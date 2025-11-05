eae0d2cb9547245ffceeb608f60e4507c0e945df
Ben <benjamin.hewer24@imperial.ac.uk>
2025-11-05 14:02:05 +0000

    Fix: Fixed the tests to include the final index of the mapping

--- composeApp/src/jvmTest/kotlin/notes/project/notes/MarkdownVisualTransformationTest.kt
+++ composeApp/src/jvmTest/kotlin/notes/project/notes/MarkdownVisualTransformationTest.kt
@@ -9,8 +9,13 @@ import kotlin.test.assertEquals
 
 class MarkdownVisualTransformationTest {
 
-    val transformation = MarkdownVisualTransformation(0)
-    val offsetMapping = transformation.MarkdownOffsetMapping()
+    var transformation = MarkdownVisualTransformation(0)
+    var offsetMapping = transformation.MarkdownOffsetMapping()
+
+    fun setCursorPosition(position: Int) {
+        transformation = MarkdownVisualTransformation(position)
+        offsetMapping = transformation.MarkdownOffsetMapping()
+    }
 
     @Test
     fun `Bold text is parsed correctly`() {
@@ -32,7 +37,7 @@ class MarkdownVisualTransformationTest {
         // Used to update mapping table.
         transformation.filter(AnnotatedString(text)).text
 
-        val originalIndices = (0..<text.length).toList()
+        val originalIndices = (0..text.length).toList()
         val transformedIndices = originalIndices.map(offsetMapping::originalToTransformed)
 
         assertEquals(expected, transformedIndices)
@@ -41,7 +46,7 @@ class MarkdownVisualTransformationTest {
     @Test
     fun `Cursor mapping is correct for plain text`() {
         val text = "This is some plain text"
-        val expected = (0..<text.length).toList()
+        val expected = (0..text.length).toList()
 
         checkCursorMappings(text, expected)
     }
@@ -49,8 +54,8 @@ class MarkdownVisualTransformationTest {
     @Test
     fun `Cursor mapping is correct for italics`() {
         val text = "This has *italic* text"
-        // 0-8 before italics, 8-14 during italics, 14-19 after italics
-        val expected = (0..8) + (8..14) + (14..19)
+        // 0-8 before italics, 8-14 during italics, 14-20 after italics
+        val expected = (0..8) + (8..14) + (14..20)
 
         checkCursorMappings(text, expected)
     }
@@ -58,8 +63,17 @@ class MarkdownVisualTransformationTest {
     @Test
     fun `Cursor mapping is correct for bold`() {
         val text = "This has **bold** text"
-        // 0-8 before bold, 8-12 during bold, 12-17 after bold
-        val expected = (0..8) + 8 + (8..12) + 12 + (12..17)
+        // 0-8 before bold, 8-12 during bold, 12-18 after bold
+        val expected = (0..8) + 8 + (8..12) + 12 + (12..18)
+
+        checkCursorMappings(text, expected)
+    }
+
+    @Test
+    fun `Cursor mapping is correct with cursor in Markdown element`() {
+        setCursorPosition(12)
+        val text = "This has **bold** text"
+        val expected = (0..text.length).toList()
 
         checkCursorMappings(text, expected)
     }
