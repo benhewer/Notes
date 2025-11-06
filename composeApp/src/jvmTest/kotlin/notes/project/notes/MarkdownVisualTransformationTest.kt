3e9c96091346a38376dcb10901305ca90237ab1f
Ben <bendhewer@gmail.com>
2025-11-06 13:37:50 +0000

    Feat: Added tests for links

--- composeApp/src/jvmTest/kotlin/notes/project/notes/MarkdownVisualTransformationTest.kt
+++ composeApp/src/jvmTest/kotlin/notes/project/notes/MarkdownVisualTransformationTest.kt
@@ -1,9 +1,11 @@
 package notes.project.notes
 
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -28,6 +30,32 @@ class MarkdownVisualTransformationTest {
                 SpanStyle(fontWeight = FontWeight.Bold),
                 5,
                 length
+            )
+        }
+        assertEquals(expected, transformedText)
+    }
+
+    @Test
+    fun `Link text is parsed correctly`() {
+        val text = "This is a [link](google.com)"
+        val transformedText = transformation.filter(AnnotatedString(text)).text
+
+        val expected = buildAnnotatedString {
+            append("This is a link")
+            addStyle(
+                SpanStyle(
+                    color = Color.Blue,
+                    textDecoration = TextDecoration.Underline
+                ),
+                10,
+                length
+            )
+            // Attach a string annotation with tag = "URL"
+            addStringAnnotation(
+                tag = "URL",
+                annotation = "google.com",
+                start = 10,
+                end = length
             )
         }
         assertEquals(expected, transformedText)
@@ -65,6 +93,15 @@ class MarkdownVisualTransformationTest {
         val text = "This has **bold** text"
         // 0-8 before bold, 8-12 during bold, 12-18 after bold
         val expected = (0..8) + 8 + (8..12) + 12 + (12..18)
+
+        checkCursorMappings(text, expected)
+    }
+
+    @Test
+    fun `Cursor mapping is correct for link`() {
+        val text = "This is a [link](google.com)"
+        // Ranges represent: "This is a ", "[link", "]", "(google.com)"
+        val expected = (0..9) + (9..13) + 13 + List("(google.com)".length) { 13 }
 
         checkCursorMappings(text, expected)
     }
